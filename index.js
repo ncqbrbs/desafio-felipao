61b032e711e69a491e2c47bd4e38d34e9c5d8eff
ncqbrbs <ncqbrbs@gmail.com>
2024-01-15 23:56:05 -0300

    modified

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-let nomeDoHeroi = "Gigi"
+let nomeDoHeroi = "GigiENana"
 let XP = 0
+let nivel = ""
 
-for(i = 0; i < 10; i++){ 
+for(i = 0; i < 5; i++){ 
     XP += 1000
-    console.log ("XP" + i)
+    console.log ("XP " + XP)
     console.log(nomeDoHeroi + " passou de nível")
 }
 
-console.log (XP)
+if(XP <= 1000){
+    nivel = "Ferro"
+}
+
+else if(XP > 1000 && XP <= 2000){
+    nivel = "Bronze"
+}
+
+else if(XP > 2000 && XP <= 5000){
+    nivel = "Prata"
+}
+
+else if(XP > 5000 && XP <= 7000){
+    nivel = "Ouro"
+}
+
+else if(XP > 7000 && XP <= 8000){
+    nivel = "Platina"
+}
+
+else if(XP > 8000 && XP <= 9000){
+    nivel = "Ascendente"
+}
+
+else if(XP > 9000 && XP <= 10000){
+    nivel = "Imortal"
+}
+
+else {
+    nivel = "Radiante"
+}
+
+console.log ("O Herói de nome " + nomeDoHeroi , "está no nível de " + nivel)
