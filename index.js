ade32fa390bf3b0c4dc6ca0cd87c68b383ca7488
ncqbrbs <ncqbrbs@gmail.com>
2024-01-15 23:50:43 -0300

    meu commit
    meu commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("oi");
+let nomeDoHeroi = "Gigi"
+let XP = 0
+
+for(i = 0; i < 10; i++){ 
+    XP += 1000
+    console.log ("XP" + i)
+    console.log(nomeDoHeroi + " passou de nível")
+}
+
+console.log (XP)
